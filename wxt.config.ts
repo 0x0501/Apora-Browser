936fa6c85abfcaaa65fc1a1b04ac5f9abce3d30a
0x0501 <41354084+0x0501@users.noreply.github.com>
2025-10-29 16:19:10 +0800

    feat: update wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
+  manifest: {
+    name: "Apora Browser",
+    version: "0.0.1",
+    description: "Browser extension for using Apora on browsers.",
+    host_permissions: ["https://apora.sumku.cc"], // Always allow Apora website
+  },
   modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   autoIcons: {
     developmentIndicator: "overlay",
